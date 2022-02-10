982630171821ba0ba1ed8d117162dbd62327c5a7
Brian S <brians@Brians-MacBook-Air.local>
2022-02-10 14:11:09 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
+import FirstComponent from './FirstComponent';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
+    <>
     <div className="App">
       <header className="App-header">
+        <FirstComponent/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is my first React app.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://bocacode.com"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Take me to bocacode
         </a>
       </header>
     </div>
+      <p>oops</p>
+      </>
   );
 }
 
---
+++ src/FirstComponent.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//good habit to have of importing react --> this is the es6 way and es5 way is with require
+//with react functions have to return jsx
+function FirstComponent(){
+    return (
+        
+        <h1>This is cool. 🤠</h1>
+        
+    )
+}
+
+export default FirstComponent;
+//exporting default is just exporting the function aka the component
